1388570f131926e6b60035999fc1eb8b4fdb4933
Jose Hugo De La Cruz Romero <hugo.delacruz@live.com.mx>
2015-04-03 14:00:36 -0600

    Add README, change code

---
+++ README.md
@@ -0,0 +1,18 @@
+FreqPy
+======
+
+Generate Wallpapers with the most frequent Chinese characters.
+
+Execution:
+
+```
+python convert.py /my/inscape/executable/path
+```
+
+
+Requirements
+------------
+
+Python 2.7+
+
+Inkscape
--- convert.py
+++ convert.py
@@ -2,193 +2,75 @@
 
 import os
 import codecs
+import sys
 
+def repl(data):
+    REPL = '{{replace}}'
+    svg_file = open('template.svg', 'r')
+    svg_data = svg_file.read()
+    svg_file.close()
 
+    total = len(data)
+    i = 0
+    while svg_data.find(REPL) > 0:
+        print svg_data.find(REPL)
+        print total, i
+        i += 1
+        if i <= total:
+            svg_data = svg_data.replace(REPL, data[i-1], 1)
+            print svg_data.find(REPL)
+    return svg_data
 
-svg_tmp1 = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-<!-- Created with Inkscape (http://www.inkscape.org/) -->
-
-<svg
-   xmlns:dc="http://purl.org/dc/elements/1.1/"
-   xmlns:cc="http://creativecommons.org/ns#"
-   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-   xmlns:svg="http://www.w3.org/2000/svg"
-   xmlns="http://www.w3.org/2000/svg"
-   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-   width="1600"
-   height="900"
-   id="svg2"
-   version="1.1"
-   inkscape:version="0.48.4 r9939"
-   sodipodi:docname="New document 1">
-  <defs
-     id="defs4" />
-  <sodipodi:namedview
-     id="base"
-     pagecolor="#ffffff"
-     bordercolor="#666666"
-     borderopacity="1.0"
-     inkscape:pageopacity="0.0"
-     inkscape:pageshadow="2"
-     inkscape:zoom="0.35"
-     inkscape:cx="588.41886"
-     inkscape:cy="520"
-     inkscape:document-units="px"
-     inkscape:current-layer="layer1"
-     showgrid="false"
-     inkscape:window-width="1600"
-     inkscape:window-height="845"
-     inkscape:window-x="-4"
-     inkscape:window-y="32"
-     inkscape:window-maximized="1" />
-  <metadata
-     id="metadata7">
-    <rdf:RDF>
-      <cc:Work
-         rdf:about="">
-        <dc:format>image/svg+xml</dc:format>
-        <dc:type
-           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
-        <dc:title></dc:title>
-      </cc:Work>
-    </rdf:RDF>
-  </metadata>
-  <g
-     inkscape:label="Layer 1"
-     inkscape:groupmode="layer"
-     id="layer1"
-     transform="translate(0,-152.36218)">
-    <rect
-       style="fill:#3d3d3d;fill-opacity:1;stroke:none"
-       id="rect2985"
-       width="1600"
-       height="900"
-       x="0"
-       y="152.36215"
-       inkscape:export-filename="C:\Users\ADMIN\Pictures\Chinese\guo4.png"
-       inkscape:export-xdpi="90"
-       inkscape:export-ydpi="90" />
-<text
-       inkscape:export-ydpi="90"
-       inkscape:export-xdpi="90"
-       inkscape:export-filename="C:\Users\ADMIN\Pictures\Chinese\guo4.png"
-       transform="matrix(1.0336891,0,0,0.96740887,-6,2)"
-       sodipodi:linespacing="125%"
-       id="text3010"
-       y="676.40704"
-       x="625.64258"
-       style="font-size:288.81243896px;font-style:normal;font-weight:normal;line-height:125%;letter-spacing:0px;word-spacing:0px;fill:#000000;fill-opacity:1;stroke:none;filter:url(#filter3806);font-family:Sans"
-       xml:space="preserve"><tspan
-         style="font-weight:bold;fill:#222222;-inkscape-font-specification:Sans Bold"
-         y="676.40704"
-         x="625.64258"
-         id="tspan3012"
-         sodipodi:role="line">"""
-
-svg_tmp1_2 = """</tspan></text>
-    <text
-       xml:space="preserve"
-       style="font-size:288.81243896px;font-style:normal;font-weight:normal;line-height:125%;letter-spacing:0px;word-spacing:0px;fill:#ffffff;fill-opacity:1;stroke:none;font-family:Sans"
-       x="627.57739"
-       y="674.33966"
-       id="text3755"
-       sodipodi:linespacing="125%"
-       transform="scale(1.0336891,0.96740887)"
-       inkscape:export-filename="C:\Users\ADMIN\Pictures\Chinese\guo4.png"
-       inkscape:export-xdpi="90"
-       inkscape:export-ydpi="90"><tspan
-         sodipodi:role="line"
-         id="tspan3757"
-         x="627.57739"
-         y="674.33966"
-         style="font-weight:bold;-inkscape-font-specification:Sans Bold">"""
-
-svg_tmp2 = """</tspan></text>
-    <text
-       transform="scale(1.0738688,0.93121245)"
-       sodipodi:linespacing="125%"
-       id="text3759"
-       y="220.49033"
-       x="36.580601"
-       style="font-size:30.56005478px;font-style:normal;font-weight:normal;line-height:125%;letter-spacing:0px;word-spacing:0px;fill:#ffffff;fill-opacity:1;stroke:none;font-family:Sans"
-       xml:space="preserve"
-       inkscape:export-filename="C:\Users\ADMIN\Pictures\Chinese\guo4.png"
-       inkscape:export-xdpi="90"
-       inkscape:export-ydpi="90"><tspan
-         y="220.49033"
-         x="36.580601"
-         id="tspan3761"
-         sodipodi:role="line">"""
-
-svg_tmp3 = """</tspan></text>
-    <text
-       xml:space="preserve"
-       style="font-size:30.56005478px;font-style:normal;font-weight:normal;line-height:125%;letter-spacing:0px;word-spacing:0px;fill:#ffffff;fill-opacity:1;stroke:none;font-family:Sans"
-       x="36.15179"
-       y="1077.9021"
-       id="text3767"
-       sodipodi:linespacing="125%"
-       transform="scale(1.0738688,0.93121245)"
-       inkscape:export-filename="C:\Users\ADMIN\Pictures\Chinese\guo4.png"
-       inkscape:export-xdpi="90"
-       inkscape:export-ydpi="90"><tspan
-         sodipodi:role="line"
-         id="tspan3769"
-         x="36.15179"
-         y="1077.9021">"""
-         
-svg_tmp4 = """</tspan></text>
-     <text
-       inkscape:export-ydpi="90"
-       inkscape:export-xdpi="90"
-       inkscape:export-filename="C:\Users\ADMIN\Pictures\Chinese\guo4.png"
-       transform="scale(1.0738688,0.93121245)"
-       sodipodi:linespacing="125%"
-       id="text2997"
-       y="1077.8339"
-       x="1326.5463"
-       style="font-size:20.56005478px;font-style:normal;font-weight:normal;line-height:125%;letter-spacing:0px;word-spacing:0px;fill:#ffffff;fill-opacity:0.2;stroke:none;font-family:Sans"
-       xml:space="preserve"><tspan
-         y="1077.8339"
-         x="1326.5463"
-         id="tspan2999"
-         sodipodi:role="line">@hugo_dc</tspan></text>
-  </g>
-</svg>
+def get_ink_path():
+    USAGE = """
+Usage:\n
+\tpython [Inkscape executable path]
 """
+    if len(sys.argv) != 2:
+        print USAGE
+        exit(1)
+    else:
+        return sys.argv[1]
 
-characters = codecs.open('characters.txt', 'r', encoding='utf-16')
 
-c = 0
-character = ''
-pinyin    = ''
-meaning   = ''
-counter = 0
-for line in characters:
-    c += 1
-    if c ==1 :
-        character = line.strip()
-    if c == 2:
-        pinyin = line.strip()
-    if c == 3:
-        counter +=1
-        meaning = line.strip()
+if __name__ == '__main__':
+    ink_path = get_ink_path()
 
-        svg_code = svg_tmp1 + character + svg_tmp1_2 + character + svg_tmp2 + pinyin + svg_tmp3 + meaning + svg_tmp4
+    characters = codecs.open('characters.txt', 'r', encoding='utf-16')
 
-        out = codecs.open('out.svg', 'w', encoding='utf-16')
-        out.write(svg_code)
-        out.close()
+    c = 0
+    character = ''
+    pinyin    = ''
+    meaning   = ''
+    counter = 0
+    for line in characters:
+        c += 1
+        if c ==1 :
+            character = line.strip()
+            continue
+        if c == 2:
+            pinyin = line.strip()
+            continue
+        if c == 3:
+            meaning = line.strip()
+            counter +=1
 
-        codecs.open('pinyin.txt', 'w', encoding='utf-16').write(pinyin)
+            svg_code = repl([character, character, pinyin, meaning])
 
-        command = 'E:\\Apps\\InkscapePortable\\App\Inkscape\\Inkscape.exe -z -f out.svg -w 1600 -j -e images\%s' % str(counter)
-        command = command + '.png'
-        print command
-        os.system(command)        
+            out = codecs.open('out.svg', 'w', encoding='utf-16')
+            out.write(svg_code)
+            out.close()
 
-        c = 0
+            codecs.open('pinyin.txt', 'w', encoding='utf-16').write(pinyin)
+
+            command = '%s -z -f out.svg -w 1600 -j -e images\%s' % (ink_path, str(counter) )
+            command = command + '.png'
+
+            print command
+
+            os.system(command)        
+
+            c = 0
 
 
 
